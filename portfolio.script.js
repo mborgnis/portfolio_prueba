85ba51e3ef94ecbd57c00560694c0fdf827b458e
mborgnis <micuborg@gmail.com>
2023-02-15 20:18:41 -0300

    cambios_15/02

---
+++ portfolio.script.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = 'Mi primer portfolio'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+/* <em>¡Mi primer portfolio!</em> */
